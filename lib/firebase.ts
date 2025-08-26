546a24242d96eca6c14324664fb01aefaa967014
Mace Akkari <maceplus@gmail.com>
2025-08-26 12:22:48 +0100

    feat: add firebase to app

---
+++ lib/firebase.ts
@@ -0,0 +1,20 @@
+import { initializeApp, getApps, getApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
+
+const firebaseConfig = {
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+};
+
+// this initialises the firebase app
+const app = !getApps().length ? initializeApp(firebaseConfig) : getApp();
+
+// this initialises initialise firebase auth
+const auth = getAuth(app);
+
+// exports auth to use in sign-in page
+export { auth };
