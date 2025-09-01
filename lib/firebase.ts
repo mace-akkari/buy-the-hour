52829c815914870dc5d7553e1e22fbd61cd1cd07
Mace Akkari <maceplus@gmail.com>
2025-09-01 12:39:10 +0100

    chore: add firestore db

--- lib/firebase.ts
+++ lib/firebase.ts
@@ -1,5 +1,6 @@
 import { initializeApp, getApps, getApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
+import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
   apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
@@ -13,8 +14,11 @@ const firebaseConfig = {
 // this initialises the firebase app
 const app = !getApps().length ? initializeApp(firebaseConfig) : getApp();
 
-// this initialises initialise firebase auth
+// this initialises firebase auth
 const auth = getAuth(app);
+
+// this initialises firestore database
+export const db = getFirestore(app);
 
 // exports auth to use in sign-in page
 export { auth };
